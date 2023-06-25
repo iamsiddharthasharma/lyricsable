093b174e1004148a8739743c0a252f1d9ad245ef
iamsiddharthasharma <siddhartha.sharma9@gmail.com>
2023-06-25 18:36:32 +0530

    Added serverless function

---
+++ lyricsable/src/search.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+export default async function handler(req, res) {
+  try {
+    const { q } = req.query;
+    const response = await axios.get('https://api.genius.com/search', {
+      headers: {
+        Authorization: 'Bearer YOUR_ACCESS_TOKEN', // Replace with your actual access token
+      },
+      params: {
+        q,
+      },
+    });
+
+    const hits = response.data.response.hits;
+    const songResults = hits.map((hit) => ({
+      title: hit.result.title,
+      artist: hit.result.primary_artist.name,
+    }));
+
+    res.status(200).json(songResults);
+  } catch (error) {
+    console.error('Error searching for songs:', error);
+    res.status(500).json({ error: 'An error occurred while searching for songs' });
+  }
+}
